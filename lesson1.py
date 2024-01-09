bdaac7cc0a42330e10ea0a46214d7c31eae1c210
Tigergeo12 <lukagamgonidze28@gmail.com>
2024-01-09 19:23:03 +0400

    first commit

---
+++ lesson1.py
@@ -0,0 +1,35 @@
+from turtle import *
+width(5)
+color("purple")
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90) #this is just the square
+
+
+forward(70)
+color("yellow")
+left(90)
+forward(130)
+right(90)
+forward(60)
+right(90)
+forward(130)#door
+
+penup()
+goto(200, 200)
+pendown()
+
+color("black")
+begin_fill()
+right(140)
+forward(160)
+left(100)
+forward(160)
+end_fill()
+
+exitonclick()
