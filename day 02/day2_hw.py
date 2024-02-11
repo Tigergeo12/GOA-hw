3280b31b948a76f2759280c46ffd88dae20e250c
Tigergeo12 <lukagamgonidze28@gmail.com>
2024-02-11 20:16:35 +0300

    day 02 homework

---
+++ day 02/day2_hw.py
@@ -0,0 +1,9 @@
+print("gavicani da davumegobrdi: nika kukavas da leqso leverashvils")
+print('''I think I made the correct decision joining the best programming academy in the world.
+      
+
+
+
+
+      
+       I thank the whole team for being so friendly and supportive and thank Nika Keshelava for making all of this happen''')
--- day 2/day2_hw.py
+++
@@ -1,2 +0,0 @@
-print("gavicani da davumegobrdi: nika kukavas da leqso leverashvils")
-print("I think I made the correct decision joining the best programming academy in the world I thank the whole team for being so friendly and supportive and thank Nika Keshelava for making all of this happen")
