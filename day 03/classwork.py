ac63bc9a0326be30cd56fa5f5a027b158590f47a
Tigergeo12 <lukagamgonidze28@gmail.com>
2024-02-15 17:01:23 +0300

    day 03

---
+++ day 03/classwork.py
@@ -0,0 +1,3 @@
+print("hello world")
+
+print("7+2")
---
+++ day 03/homework.py
@@ -0,0 +1,12 @@
+age_brother = 11
+age_mom = 40
+age_dad = 41
+print("mom " + str(age_mom))
+print("dad " + str(age_dad))
+print("brother " + str(age_brother))
+
+print('''
+mom in 2 years''' + str(age_mom + 2))
+print("dad in 2 years" + str(age_dad + 2))
+print("brother in 2 years" + str(age_brother + 2))
+
