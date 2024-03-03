1631fa6fab3570d135921056074c59c1b55e9c76
Tigergeo12 <lukagamgonidze28@gmail.com>
2024-03-03 20:03:33 +0300

    day 08

---
+++ day 08/classwork.py
@@ -0,0 +1,4 @@
+for i in range(5):
+    print("luka")
+
+print('1234566')
---
+++ day 08/hw1.py
@@ -0,0 +1,7 @@
+name = input("whats your name? :")
+password = input("enter your password: ")
+repeat_password = input("re-enter your password: ")
+if password == repeat_password:
+    print("tqven warmatebit gaiaret registracia")
+else:
+    print("paroli arasworia")
